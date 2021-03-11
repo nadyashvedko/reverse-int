5279bcfa38dee2913a42f7a1e5344105f1852a24
Nadzeya Molchan <nadzeya_molchan@epam.com>
2021-03-11 16:06:28 +0300

    init: add solution task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let digit, result = 0
+
+    while( n ){
+        digit = n % 10  //  Get right-most digit. Ex. 123/10 → 12.3 → 3
+        result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
+        n = n/10|0  //  Remove right-most digit. Ex. 123 → 12.3 → 12
+    }
+
+    return Math.abs(result);
 }
